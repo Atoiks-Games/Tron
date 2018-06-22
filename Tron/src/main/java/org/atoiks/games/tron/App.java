bd88a53b51ef0580160da600bdd31a51accdc9ad
Paul Teng <plankp@outlook.com>
2018-06-22 12:15:05 -0400

    Update license

--- Tron/src/main/java/org/atoiks/games/tron/App.java
+++ Tron/src/main/java/org/atoiks/games/tron/App.java
@@ -1,3 +1,10 @@
+/**
+ *   Tron  Copyright (C) 2018  Atoiks-Games <atoiks-games@outlook.com>
+ *   This program comes with ABSOLUTELY NO WARRANTY;
+ *   This is free software, and you are welcome to redistribute it
+ *   under certain conditions;
+ */
+
 package org.atoiks.games.tron;
 
 import org.atoiks.games.framework2d.IFrame;
